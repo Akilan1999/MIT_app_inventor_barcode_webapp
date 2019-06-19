0d250d3eb3b17a1219b404a7b1fb92bcc744fafd
Akilan1999 <Akilanselva@hotmail.com>
2019-06-19 11:22:34 +0400

    Seversion 1 ready

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,14 @@
-var DB_config = require('../../config/database.js');
+//var DB_config = require('../../config/database.js');
+var request = require('request');
 
 
 
-exports.relay = function(req, res) {
 
-//Takes parameters from GET request
- var Bar_code_num = req.query.barcode;
-
- DB_config.connection.query("insert into bar_code(barcode) values(?);",[Bar_code_num],
- function (err, result, fields) {
-
-  if (err) throw err;
-
- });
+exports.home = function(req, res) {
+  if (!(req.session.barcode)) {
+       req.session.barcode = [];
+   }
+  res.render('barcode_info.ejs',{barcodes: req.session.barcode});
 }
 
  exports.Render_barcode = function(req, res) {
@@ -22,11 +18,42 @@ exports.relay = function(req, res) {
 
   var barcode;
 
-  if (!(req.session.barcode)) {
-       req.session.barcode = [];
-   }
-
   req.session.barcode.push(Bar_code_num);
+  console.log(req.session.barcode);
+  res.redirect('/');
+}
 
-  res.render('barcode_info.ejs',{barcodes: req.session.barcode});
+exports.delete  = function(req, res) {
+  //Delete record from the array
+   console.log(req.query.id);
+   if(req.query.id == 0){
+     req.session.barcode.shift();
+   }
+   else{
+   req.session.barcode.splice(req.query.id,req.query.id);
+   }
+   res.redirect('/');
+}
+
+exports.send = function(req, res) {
+
+  for(var i = 0; i < req.session.barcode.length; i++){
+
+  request({
+	uri: 'http://afaf.fortidyndns.com:81/barcodeReader.aspx',
+	qs: {
+		refNo: req.session.barcode[i]
+	},
+	function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			return response;
+		} else {
+			return error;
+		}
+	}
+});
+}
+
+res.redirect('/');
+
 }
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 var home = require('../app/controllers/home');
 
   module.exports = function (app){
-  app.get('/transfer', home.relay);
-  app.get('/lol',home.Render_barcode); 
+  app.get('/',home.home);
+  app.get('/barcode',home.Render_barcode);
+  app.get('/delete',home.delete);
+  app.get('/send',home.send);
  }
